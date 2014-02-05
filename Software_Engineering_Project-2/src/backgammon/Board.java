e68c752d23eeb26a39c0841ceb95b6700e177f20
YEg0r <egor.lobaskin@ucdconnect.ie>
2014-02-05 16:22:16 +0000

    final commit on this assignment on my part, the rest is for the team

--- Software_Engineering_Project-2/src/backgammon/Board.java
+++ Software_Engineering_Project-2/src/backgammon/Board.java
@@ -4,6 +4,7 @@ public class Board
 {
 	static final int NUMBER_OF_PEGS = 24;				// initialising the constants here
 	static final int BAR = 2;
+	static final int OFF = 2;
 	
 	/*---------------------------------------------------------------------*/
 	static final int RED_HOME_START = 18;
@@ -19,43 +20,30 @@ public class Board
 	 * adjusted for an array index starting at 0
 	 */
 	
-	static final String GREATER_THAN = ">";
-	static final String LESS_THAN = "<";
 	static final String UPPPER_LINE_OF_BOARD = "13----+----+----+----+----18  BAR  19----+----+----+----+----24 OFF\n";
 	static final String BOTTOM_LINE_OF_BOARD = "12----+----+----+----+----07  BAR  06----+----+----+----+----01 OFF\n";
 	// end constants
 	
-	int playingBoard[] = new int[NUMBER_OF_PEGS + BAR];
+	int playingBoard[] = new int[NUMBER_OF_PEGS + BAR + OFF];
 	
 	public String showBlackBar()
 	{
-		return "  X" + playingBoard[25] + " ";
+		return "  X" + playingBoard[25] + "  ";
+	}
+	
+	public String showBlackOff()
+	{
+		return "X" + playingBoard[27] + "\n";
 	}
 	
 	public String showWhiteBar()
 	{
-		return "  O" + playingBoard[24] + " ";
+		return "  O" + playingBoard[24] + "  ";
 	}
 	
-	/**
-	 * this method is used in one of our nested loops to have a variable looping
-	 * condition
-	 * 
-	 * @param numberOne
-	 * @param comparator
-	 *            the operation to be performed on the numbers
-	 * @param numberTwo
-	 * @return true/false
-	 */
-	private boolean compareNumbers( int numberOne, String comparator,
-			int numberTwo )
+	public String showWhiteOff()
 	{
-		boolean returnValue = false;
-		if ( comparator.equals ( "<" ) )
-			returnValue = ( numberOne < numberTwo );
-		else if ( comparator.equals ( ">" ) )
-			returnValue = ( numberOne > numberTwo );
-		return returnValue;
+		return "O" + playingBoard[26] + "\n\n";
 	}
 	
 	/**
@@ -101,69 +89,64 @@ public class Board
 		}
 	}
 	
+	public String printMen( int numberOfMen )
+	{
+		String returnValue;
+		if ( numberOfMen < 0 )
+			returnValue = ( "X" + Math.abs ( numberOfMen ) + "  " );	// converts negative numbers to black men
+		else if ( numberOfMen > 0 )
+			returnValue = ( "O" + numberOfMen + "  " );					// converts positive numbers to white men
+		else returnValue = ( " |  " );
+		return returnValue;
+	}
+	
 	/**
 	 * self-explanatory method
 	 */
 	public void printBoard()
 	{
-		int start, limit, increment;			// start, end and increment of nested loop. they change depending on the value of the outer loop's counter
+		String boardPositionCorrection;				// adds whitespaces before men on the display, for a neater look
 		
 		System.out.print ( UPPPER_LINE_OF_BOARD );
-		for ( int quarterOfBoard = 0; quarterOfBoard < 4; quarterOfBoard++ )	// outer loop of nested loop
+		
+		// print the top right quarter of the board
+		for ( int counter = TOP_OUTER_START; counter < TOP_OUTER_END; counter++ )
 		{
-			String endOfLine = "";		// this variable will contain either the "Bar" or "Off"
-			String compareOperation = "";		// used to change the condition on the inner loop
-			// the two variables above ^ need to be re-initialised every run of the loop
-			switch ( quarterOfBoard )
-			{
-				case 0:
-					start = TOP_OUTER_START;				// setting parameters for inner loop
-					limit = TOP_OUTER_END;					//
-					endOfLine += showBlackBar ();			//
-					increment = 1;							//
-					compareOperation += LESS_THAN;				//
-					break;
-				case 1:
-					start = RED_HOME_START;
-					limit = RED_HOME_END;
-					endOfLine += "\n\n";					// insert off() method here later
-					increment = 1;
-					compareOperation += LESS_THAN;
-					break;
-				case 2:
-					start = BOTTOM_OUTER_START;				// setting parameters for inner loop
-					limit = BOTTOM_OUTER_END;				//
-					endOfLine += showWhiteBar ();			//
-					increment = -1;							//
-					compareOperation += GREATER_THAN;				//
-					break;
-				default:
-					start = WHITE_HOME_START;
-					limit = WHITE_HOME_END;
-					endOfLine += "\n";					// insert off() method here later
-					increment = -1;
-					compareOperation += GREATER_THAN;
-					break;
-			}
-			for ( int counter = start; compareNumbers ( counter,
-					compareOperation, limit ); counter += increment )		// this nested loop is mentioned with the compareNumbers() method above
-			{
-				int numberOfMen = playingBoard[counter];
-				String board_Position_Correction = ( ( ( counter == 23 ) || ( counter == 0 ) ) ? " "
-						: "" )
-						+ ( ! ( ( counter == 12 ) || ( counter == 11 ) ) ? " "
-								: "" );
-				// this variable above ^ adjusts the exact position of the men on the printed-out board, for a neater look
-				if ( numberOfMen < 0 )
-					System.out.print ( board_Position_Correction + "X"
-							+ Math.abs ( numberOfMen ) + "  " );	// converts negative numbers to black men
-				else if ( numberOfMen > 0 )
-					System.out.print ( board_Position_Correction + "O"
-							+ numberOfMen + "  " );					// converts positive numbers to white men
-				else System.out.print ( "  |  " );
-			}
-			System.out.print ( endOfLine );
+			boardPositionCorrection = ( ( counter == 12 ) ? "" : " " );
+			System.out.print ( boardPositionCorrection
+					+ printMen ( playingBoard[counter] ) );
 		}
+		System.out.print ( showBlackBar () );
+		
+		// print the top left quarter of the board		
+		for ( int counter = RED_HOME_START; counter < RED_HOME_END; counter++ )
+		{
+			boardPositionCorrection = ( ( counter == 18 ) ? "" : " " )
+					+ ( ( counter == 23 ) ? " " : "" );
+			System.out.print ( boardPositionCorrection
+					+ printMen ( playingBoard[counter] ) );
+		}
+		System.out.print ( showWhiteOff () );
+		
+		// print the bottom right quarter of the board
+		for ( int counter = BOTTOM_OUTER_START; counter > BOTTOM_OUTER_END; counter-- )
+		{
+			boardPositionCorrection = ( ( counter == 11 ) ? "" : " " );
+			System.out.print ( boardPositionCorrection
+					+ printMen ( playingBoard[counter] ) );
+		}
+		System.out.print ( showWhiteBar () );
+		
+		// print the bottom left quarter of the board
+		for ( int counter = WHITE_HOME_START; counter > WHITE_HOME_END; counter-- )
+		{
+			boardPositionCorrection = ( ( counter == 5 ) ? "" : " " )
+					+ ( ( counter == 0 ) ? " " : "" );
+			System.out.print ( boardPositionCorrection
+					+ printMen ( playingBoard[counter] ) );
+		}
+		System.out.print ( showBlackOff () );
+		
 		System.out.print ( BOTTOM_LINE_OF_BOARD );
 	}
 	
